d8e8f9bfc24e5e72bcd5764291ca7020f00d7bd2
Kevin Liu <kliu33@buffalo.edu>
2023-02-20 14:56:42 -0500

    game rendering

--- frontend/public/game.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const canvas = document.getElementById("game-canvas");
-  const ctx = canvas.getContext("2d");
-  ctx.fillStyle = "cornflowerblue";
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-});
