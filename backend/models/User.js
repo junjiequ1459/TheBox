76a17eca4718a8500475e659d240cb7c95b5ded0
rexqu <junjiequ123@gmail.com>
2023-02-21 16:47:08 -0500

    work on amazon s3

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,21 +1,28 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  username: {
-    type: String,
-    required: true
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    profileImageUrl: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    hashedPassword: {
+      type: String,
+      required: true,
+    },
   },
-  email: {
-    type: String,
-    required: true
-  },
-  hashedPassword: {
-    type: String,
-    required: true
+  {
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
-module.exports = mongoose.model('User', userSchema);
+module.exports = mongoose.model("User", userSchema);
--- frontend/src/components/ProfilePage/ProfilePage.js
+++ frontend/src/components/ProfilePage/ProfilePage.js
@@ -1,11 +1,12 @@
 import { useParams } from "react-router-dom";
-
-function ProfilePage () {
+import ConsoleNavBar from "../ConsoleNavBar/ConsoleNavBar";
+function ProfilePage() {
   const { userId } = useParams();
   //Start of a profile page, need users in the store
   return (
     <>
-      <h1>Profile</h1>
+      <ConsoleNavBar name={"about"} />
+      <div className="console-container"> </div>
     </>
   );
 }
