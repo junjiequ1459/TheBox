42d6b9c81823185894bd00d4a1ea0e5c859eae73
rexqu <junjiequ123@gmail.com>
2023-02-21 13:04:03 -0500

    fix css for sign in and sign up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { Switch } from "react-router-dom";
 
-import { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { Switch } from 'react-router-dom';
+import { AuthRoute, ProtectedRoute } from "./components/Routes/Routes";
+import NavBar from "./components/NavBar/NavBar";
+import Video from "./components/VideoBackground/Video";
+import MainPage from "./components/MainPage/MainPage";
+import LoginForm from "./components/SessionForms/LoginForm";
+import SignupForm from "./components/SessionForms/SignupForm";
+import GamePage from "./components/GamePage/GamePage";
+import AboutPage from "./components/AboutPage/AboutPage";
 
-import { AuthRoute, ProtectedRoute } from './components/Routes/Routes';
-import NavBar from './components/NavBar/NavBar';
-import Video from './components/VideoBackground/Video';
-import MainPage from './components/MainPage/MainPage';
-import LoginForm from './components/SessionForms/LoginForm';
-import SignupForm from './components/SessionForms/SignupForm';
-import GamePage from './components/GamePage/GamePage';
-
-import { getCurrentUser } from './store/session';
+import { getCurrentUser } from "./store/session";
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -22,14 +22,14 @@ function App() {
   }, [dispatch]);
   return (
     <>
-      <Video/>
+      <Video />
       <NavBar />
       <Switch>
         <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/about" component={AboutPage} />
         <AuthRoute exact path="/login" component={LoginForm} />
         <AuthRoute exact path="/signup" component={SignupForm} />
         <AuthRoute exact path="/play" component={GamePage} />
-
       </Switch>
     </>
   );
