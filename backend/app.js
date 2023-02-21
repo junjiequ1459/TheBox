97e01efa7845360c340856752c362d714e6d32a0
zahiahmed <zahi.ahmed33@gmail.com>
2023-02-21 11:54:28 -0500

    start room sockets

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,10 @@
 const express = require("express");
+const http = require("http");
+const socketIO = require("socket.io");
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const debug = require("debug");
@@ -15,8 +21,6 @@ const roomsRouter = require("./routes/api/rooms");
 const usersRouter = require("./routes/api/users");
 const csrfRouter = require("./routes/api/csrf");
 const gameRouter = require("./routes/api/games");
-
-const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -44,12 +48,6 @@ app.use("/api/rooms", roomsRouter);
 app.use("/api/csrf", csrfRouter);
 app.use("/api/games", gameRouter);
 
-// app.get("/CSRF-TOKEN", (req, res) => {
-//   const csrfToken = req.csrfToken();
-//   res.cookie("CSRF-TOKEN", csrfToken);
-//   res.json({ "CSRF-Token": csrfToken });
-// });
-
 app.use((req, res, next) => {
   const err = new Error("Not Found");
 
@@ -70,5 +68,28 @@ app.use((err, req, res, next) => {
   });
 });
 
+io.on("connection", (socket) => {
+  console.log("a user connected");
+
+  socket.on("join", (room) => {
+    socket.join(room);
+    console.log(`user joined room ${room}`);
+  });
+
+  socket.on("leave", (room) => {
+    socket.leave(room);
+    console.log(`user left room ${room}`);
+  });
+
+  // socket.on("message", (data) => {
+  //   io.to(data.room).emit("message", data.message);
+  //   console.log(`user sent message to room ${data.room}`);
+  // });
+
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
 
 module.exports = app;
