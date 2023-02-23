e56962ae74eeaac90e44480c27c89258eab5de13
rexqu <junjiequ123@gmail.com>
2023-02-23 11:20:24 -0500

    Animation for refresh and fix amazon s3

---
+++ frontend/src/LoadingScreen/LoadingScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./LoadingScreen.css";
+
+function LoadingScreen() {
+  return (
+    <div className="loading-screen-container">
+      <div className="loading-screen-shape-container">
+        <div className="loading-screen-shape"></div>
+        <div className="loading-screen-shape2"></div>
+        <div className="loading-screen-shape3"></div>
+      </div>
+    </div>
+  );
+}
+
+export default LoadingScreen;
