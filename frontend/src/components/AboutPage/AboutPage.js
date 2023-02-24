8d18dd62f37057d9569150b6f381b61ea73bc872
ZahiAhmed <zahi.ahmed33@gmail.com>
2023-02-24 01:28:16 -0500

    about page edits

--- frontend/src/components/AboutPage/AboutPage.js
+++ frontend/src/components/AboutPage/AboutPage.js
@@ -16,14 +16,14 @@ function AboutPage() {
             </div>
             <div>
               <a
-                href="https://github.com/junjiequ1459/TheBox"
+                href="https://github.com/ZahiAhmed"
                 target="_blank"
                 rel="noreferrer"
               >
                 <i className="fa-brands fa-github"></i>
               </a>
               <a
-                href="https://www.linkedin.com/in/junjie-qu-239070169/"
+                href="https://www.linkedin.com/in/zahiahmed/"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -33,7 +33,9 @@ function AboutPage() {
                 <i className="fa-brands fa-angellist"></i>
               </a>
             </div>
-            <h2>Frontend/Team Lead</h2>
+            <h2>Backend Lead
+            <br/> EXPRESS COOK
+            </h2>
           </div>
           <div className="image-icon-container">
             <div id="img-div">
@@ -46,14 +48,14 @@ function AboutPage() {
             </div>{" "}
             <div>
               <a
-                href="https://github.com/junjiequ1459/TheBox"
+                href="https://github.com/kliu33"
                 target="_blank"
                 rel="noreferrer"
               >
                 <i className="fa-brands fa-github"></i>
               </a>
               <a
-                href="https://www.linkedin.com/in/junjie-qu-239070169/"
+                href="https://www.linkedin.com/in/kevin-liu-3a5b96158/"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -63,7 +65,9 @@ function AboutPage() {
                 <i className="fa-brands fa-angellist"></i>
               </a>
             </div>
-            <h2>Fullstack Flex</h2>
+            <h2>Frontend Lead
+            <br/> ICE SPICE COOK
+            </h2>
           </div>
           <div className="image-icon-container">
             <div id="img-div">
@@ -75,7 +79,7 @@ function AboutPage() {
             </div>{" "}
             <div>
               <a
-                href="https://github.com/junjiequ1459/TheBox"
+                href="https://github.com/junjiequ1459"
                 target="_blank"
                 rel="noreferr er"
               >
@@ -92,7 +96,9 @@ function AboutPage() {
                 <i className="fa-brands fa-angellist"></i>
               </a>
             </div>
-            <h2>Fullstack Flex</h2>
+            <h2>Team Lead/Frontend
+              <br/> CSS AWS COOK
+            </h2>
           </div>
           <div className="image-icon-container">
             <div id="img-div">
@@ -105,14 +111,14 @@ function AboutPage() {
             </div>
             <div>
               <a
-                href="https://github.com/junjiequ1459/TheBox"
+                href="https://github.com/joeytsui1"
                 target="_blank"
                 rel="noreferrer"
               >
                 <i className="fa-brands fa-github"></i>
               </a>
               <a
-                href="https://www.linkedin.com/in/junjie-qu-239070169/"
+                href="https://www.linkedin.com/in/joey-tsui-5836a2240/"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -122,25 +128,22 @@ function AboutPage() {
                 <i className="fa-brands fa-angellist"></i>
               </a>
             </div>
-            <h2>Backend Lead</h2>
+            <h2>FullStack Lead
+            <br/> SOCKET COOK
+            </h2>
           </div>
         </div>
         <div className="team-member-description">
           <div className="about-description">
-            Welcome! My name is [Your Name], and I am [Your Age] years old. I am
-            [Your Occupation or Current Pursuit], and I am passionate about
-            [Your Interests or Goals]. Throughout my life, I have been driven by
-            a desire to learn, grow, and make a positive impact on the world
-            around me. [Add any personal details, achievements, or experiences
-            that you feel are relevant]. I am excited to [Connect with/Provide
-            Value to/Share my experiences with] the
-            [Community/Organization/Platform] and look forward to [Any Goals or
-            Objectives]. Thank you for taking the time to get to know me, and I
-            hope we can work together to achieve great things.
+            Welcome! I'm 25 years old, and my name is Zahi Ahmed. Being a
+            Full-Stack Software Engineer for Aspring, coding is my life's work.
+            My motivation in life has always been to a drive to develop, learn,
+            and have a constructive influence on the world and I'm a computer
+            science graduate.
           </div>
           <div className="about-description">
-            Welcome! My name is [Kevin Liu], and I am [Your Age] years old. I am
-            [Aspiring Software Engineer], and I am passionate about [life].
+            Welcome! My name is Kevin Liu, and I am 25 years old. I am
+            Aspiring Software Engineer, and I am passionate about life.
             Throughout my life, I have been driven by a desire to learn, grow,
             and make a positive impact on the world around me. [Add any personal
             details, achievements, or experiences that you feel are relevant]. I
@@ -150,12 +153,12 @@ function AboutPage() {
             me, and I hope we can work together to achieve great things.
           </div>
           <div className="about-description">
-            Hello! My name is JunJieQu(Rex), and I am 23 years old. I am an
-            Aspiring SWE. Throughout my life, I have been driven by a desire to
-            learn, grow, and make a positive impact on the world around me. I am
-            excited to Share My experieces throughout my time at App Academy
-            Thank you for taking the time to get to know me, and I hope we can
-            work together to achieve great things.
+            Hello! JunJieQu(Rex) is my name, and I am 23 years old. I am a
+            future SWE. I've been motivated to study, develop, and have a
+            positive impact on the world all of my life. I am eager to discuss
+            my experiences while attending App Academy. I appreciate you taking
+            the time to get to know me, and I look forward to working with you
+            to accomplish great things.
           </div>
           <div className="about-description">
             Welcome! My name is [Your Name], and I am [Your Age] years old. I am
