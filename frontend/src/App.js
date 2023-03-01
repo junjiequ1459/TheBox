a62a838a934a3848491b040eaf140684dab54cde
ZahiAhmed <zahi.ahmed33@gmail.com>
2023-02-28 19:49:08 -0500

    bug fix 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -46,9 +46,9 @@ function App() {
             <AuthRoute exact path="/login" component={LoginForm} />
             <AuthRoute exact path="/signup" component={SignupForm} />
             <Route exact path="/about" component={AboutPage} />
-            <Route exact path="/roomform" component={RoomForm} />
-            <Route exact path="/roomlist" component={RoomList} />
-            <Route path="/room/:roomId" component={RoomShowPage} />
+            <ProtectedRoute exact path="/roomform" component={RoomForm} />
+            <ProtectedRoute exact path="/roomlist" component={RoomList} />
+            <ProtectedRoute path="/room/:roomId" component={RoomShowPage} />
             <ProtectedRoute exact path="/" component={MainPage} />
             <ProtectedRoute exact path="/play" component={GamePage} />
             <ProtectedRoute path="/profile/:userId" component={ProfilePage} />
